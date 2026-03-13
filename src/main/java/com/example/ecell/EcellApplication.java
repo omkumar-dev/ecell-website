e5788d9f563c374716908dfd9561298b9ec703ce
Om Rana <omrana@Oms-MacBook-Air.local>
2026-03-13 23:17:44 +0530

    E-Cell website working on port 9090

---
+++ src/main/java/com/example/ecell/EcellApplication.java
@@ -0,0 +1,11 @@
+package com.example.ecell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcellApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcellApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/ecell/EcellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
