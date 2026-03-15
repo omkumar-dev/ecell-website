4a08015d4eb008ab2aad1ee5435321c8e79f226d
OM KUMAR <omjee7777@gmail.com>
2026-03-15 22:46:44 +0530

    Create RegistrationRepository.java

---
+++ src/main/java/com/example/ecell/RegistrationRepository.java
@@ -0,0 +1,8 @@
+package com.example.ecell;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {
+}
